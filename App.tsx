f3ef9d99ec7f3860f88dc8346318a1c8ca947a9c
NiraphatK <niraphat23@gmail.com>
2024-07-27 10:24:27 +0700

    Practice1_Ch04_ReactHook[July,27,2024]

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Alert } from "react-native";
-import AppFooter from "./components/AppFooter";
-import AppHeader from "./components/AppHeader";
-import Content from "./components/Content";
+import { View, Text } from "react-native";
+import React from "react";
+import ProfileScreen from "./components/ProfileScreen";
 
-export default function App(): React.JSX.Element {
+const App = (): React.JSX.Element => {
   return (
-    <View style={styles.container}>
-      <AppHeader name="Niraphat Konghuayrob" msg="Message from App.tsx" />
-      <Content msg="Message from App.tsx" name="Niraphat Konghuayrob" />
-      <AppFooter title="Thai-Nichi Institute of Technology" />
-      <StatusBar style="auto" />
+    <View>
+      <ProfileScreen />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-});
+export default App;
---
+++ styles/styles.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+
+export default StyleSheet.create({
+  container: {
+    alignItems: "center",
+    padding: 20,
+    backgroundColor: "#dddddd",
+  },
+  profileImage: {
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+    marginRight: 20,
+    borderWidth: 3,
+    borderColor: "#3d3d3d3d",
+  },
+  profileContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: "white",
+    elevation: 5,
+    marginTop: 50,
+  },
+  profileName: {
+    color: "#3d3d3d",
+    fontWeight: "500",
+  },
+});
